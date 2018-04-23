b5c1b1918b907dd287a7eaf1d1e10843dee5d941
Blake Blackwell <16102658+PythonPsychonaut@users.noreply.github.com>
2018-04-23 11:27:36 -0400

    Update BinaryTree.h
    Patched

--- PC_19/PC_19_1/BinaryTree.h
+++ PC_19/PC_19_1/BinaryTree.h
@@ -7,29 +7,25 @@ using namespace std;
 class BinaryTree
 {
 private:
-	// The BtreeNode class is used to build the tree
-	class BtreeNode
+	class CatstreeNode
 	{
 		friend class BinaryTree;
 		double value;
-		BtreeNode *left;
-		BtreeNode *right;
-		BtreeNode(double value1, BtreeNode *left1 = NULL, BtreeNode *right1 = NULL)
+		CatstreeNode *left;
+		CatstreeNode *right;
+		CatstreeNode(double value1, CatstreeNode *left1 = NULL, CatstreeNode *right1 = NULL)
 		{
 			value = value1;
 			left = left1;
 			right = right1;
 		}
 	};
-	BtreeNode *root;		// Pointer to the root of the tree
-	// Various helper functions.
-	void insert(BtreeNode *&, double);
-	bool search(double, BtreeNode *);
-	void fillInorder(vector <double> & v, BtreeNode *);
+	CatstreeNode *root;		
+	void insert(CatstreeNode *&, double);
+	bool search(double, CatstreeNode *);
+	void fillInorder(vector <double> & v, CatstreeNode *);
 public:
-	// Constructor
 	BinaryTree(){ root = NULL; }
-	// Member functions.
 	void insert(double x)
 	{insert(root, x);}
 	bool search(double x)
