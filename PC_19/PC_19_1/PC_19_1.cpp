0799cf6f8f15219f042ab0bf154f47be1d4de39b
Blake Blackwell <16102658+PythonPsychonaut@users.noreply.github.com>
2018-04-23 11:23:59 -0400

    Update PC_19_1.cpp
    Fixed

--- PC_19/PC_19_1/PC_19_1.cpp
+++ PC_19/PC_19_1/PC_19_1.cpp
@@ -1,23 +1,6 @@
-/*
-Simple Binary Search Tree Class
-Write a class for implementing a simple binary search tree capable of storing numbers. The
-class should have member functions
 
-	void insert(double x)
-	bool search(double x)
-	void inorder(vector <double> & v )
-
-The insert function should not use recursion directly, or indirectly by calling a recursive
-function. The search function should work by calling a private recursive member function
-
-	bool search(double x, BtreeNode *t)
-
-The inorder function is passed an initially empty vector v: it fills v with the inorder list of
-numbers stored in the binary search tree. Demonstrate the operation of the class using a
-suitable driver program.
-*/
 #include <iostream>
-#include "BinaryTree.h"
+#include <BinaryTree.h>
 #include <vector>
 using namespace std;
 
@@ -32,10 +15,10 @@ int main()
 	tree.insert(12.9);
 	tree.insert(9.7);
 
-	if (tree.search(3))
-		cout << "3 was found in tree.\n";
+	if (tree.search(8))
+		cout << "8 was found in tree.\n";
 	else
-		cout << "3 was not found in tree.\n";
+		cout << "8 was not found in tree.\n";
 
 	tree.inorder(treeValues);
 
