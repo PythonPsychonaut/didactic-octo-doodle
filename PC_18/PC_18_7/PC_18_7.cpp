a0f4442f3ff280dfde844776683b85a6580d31c7
Blake Blackwell <16102658+PythonPsychonaut@users.noreply.github.com>
2018-04-23 11:19:50 -0400

    Update PC_18_7.cpp
    Changed queue amount.

--- PC_18/PC_18_7/PC_18_7.cpp
+++ PC_18/PC_18_7/PC_18_7.cpp
@@ -1,29 +1,17 @@
-/*
-Queue Exceptions
-Modify the static queue class used in Program 18-5 as follows.
- 1. Make the isFull member private.
- 2. Define a queue overflow exception and modify enqueue so that it throws this exception
- 	when the queue runs out of space.
- 3. Define a queue underflow exception and modify dequeue so that it throws this exception
- 	when the queue is empty.
- 4. Rewrite the main program so that it catches overflow exceptions when they occur.
- 	The exception handler for queue overflow should print an appropriate error message
-	and then terminate the program.
-*/
+
 #include <iostream>
-#include "IntQueue.h"
+#include <IntQueue.h>
 using namespace std;
 
 int main()
 {
-	IntQueue iQueue(5);
+	IntQueue iQueue(7);
 
-	cout << "Enqueuing 5 items...\n";
+	cout << "Enqueuing 7 items...\n";
 
-	// Enqueue 5 items.
 	try
 	{
-		for (int k = 1; k <= 6; k++)
+		for (int k = 1; k <= 8; k++)
 			iQueue.enqueue(k*k);
 	}
 	catch (IntQueue::OverFlow)
@@ -32,7 +20,6 @@ int main()
 		exit(1);
 	}
 
-	// Dequeue and retrieve all items in the queue.
 	cout << "The values in the quere were: ";
 	while (!iQueue.isEmpty())
 	{
